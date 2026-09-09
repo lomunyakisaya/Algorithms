90e18a88b4a7e29dd95a3ad41ff7f459359f928c
lomunyakisaya1@gmail.com <lomunyak@LDOS.ldos>
2026-09-09 10:05:04 +0300

    Modular Programming

---
+++ C++/oop/calculator.cpp
@@ -0,0 +1,21 @@
+#include "calculator.h"
+
+int calculator::add(int a, int b)
+{
+    return a + b;
+}
+
+int calculator::subtract(int a, int b)
+{
+    return a - b;
+}
+
+int calculator::multiply(int a, int b)
+{
+    return a * b;
+}
+
+int calculator::divide(int a, int b)
+{
+    return a / b;
+}
